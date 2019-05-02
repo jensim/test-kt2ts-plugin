9c4616a2f2d12433b6323b42d036ac9c08a23de6
Jens Brimfors <jens.brimfors@nordnet.se>
2019-05-02 08:57:13 +0200

    Added the missing dependency to ts-generator and its repo

--- kotlin-style/build.gradle.kts
+++ kotlin-style/build.gradle.kts
@@ -1,3 +1,11 @@
+buildscript {
+    repositories {
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.github.ntrrgc:ts-generator:1.1.1")
+    }
+}
 plugins {
     id("kotlin2ts.kt2ts") version "1.0.0"
 }
